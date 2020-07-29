86ad673087e51539348e15f6925579e3ff8bdf7e
fessfoodies <68938944+fessfoodies@users.noreply.github.com>
2020-07-29 20:38:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def start():
                 id = dms[i]['id']
 
                 if len(message) is not 0 and len(message) < 280:
-                    if "foodies!" or "Foodies!" in message:
+                    if "foodies!" in message:
                         #message = message.replace("foodies", "")
                         if len(message) is not 0:
                             if dms[i]['media'] is None:
